9dde218a2be1e35e56823e33e657bef39c862124
Angela <angelabaaity@outlook.com>
2025-12-22 15:33:11 +0200

    updates

--- admin-assessment.php
+++ admin-assessment.php
@@ -43,7 +43,7 @@ if ($editMode) {
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
     <link rel="stylesheet" href="styles/navbar.css">
-    <link rel="stylesheet" href="styles/admin-assessments.css?v=8">
+    <link rel="stylesheet" href="styles/admin-assessments.css?v=4">
 </head>
 
 <body>
