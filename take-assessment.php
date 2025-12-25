be920585cadc4551341908667ea90280b701f6c0
Angela <angelabaaity@outlook.com>
2025-12-25 11:45:52 +0200

    chat bot

--- take-assessment.php
+++ take-assessment.php
@@ -97,4 +97,7 @@ $questions_result = $db->query($questions_sql);
 
 </html>
 
+<script>
+  (function () { if (!window.chatbase || window.chatbase("getState") !== "initialized") { window.chatbase = (...arguments) => { if (!window.chatbase.q) { window.chatbase.q = [] } window.chatbase.q.push(arguments) }; window.chatbase = new Proxy(window.chatbase, { get(target, prop) { if (prop === "q") { return target.q } return (...args) => target(prop, ...args) } }) } const onLoad = function () { const script = document.createElement("script"); script.src = "https://www.chatbase.co/embed.min.js"; script.id = "RxU2NixvO8dnE0x3jXRA7"; script.domain = "www.chatbase.co"; document.body.appendChild(script) }; if (document.readyState === "complete") { onLoad() } else { window.addEventListener("load", onLoad) } })();
+</script>
 <script src="script/script.js" defer></script>
