3283e47611be3327b7337bad6c4a4a65caada53c
Angela <angelabaaity@outlook.com>
2026-01-03 22:45:08 +0200

    .

--- dashboard.php
+++ dashboard.php
@@ -24,8 +24,8 @@ if ($filter === 'pending') {
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-    <link rel="stylesheet" href="styles/navbar1.css?v=5">
-    <link rel="stylesheet" href="styles/dashboard.css?v=7">
+    <link rel="stylesheet" href="styles/navbar1.css">
+    <link rel="stylesheet" href="styles/dashboard.css?v=11">
 </head>
 
 <body>
